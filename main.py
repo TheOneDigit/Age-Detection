329e6013a38d4d6371b6265db2a3b62dbfb069c6
Aqdas Ansari <113194530+Aqdas101@users.noreply.github.com>
2024-08-06 22:03:43 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 from age_recognition import age_gender_detector, getFaceBox
 
-faceProto = "modelNweight/opencv_face_detector.pbtxt"
-faceModel = "modelNweight/opencv_face_detector_uint8.pb"
+faceProto = "Model Weight/opencv_face_detector.pbtxt"
+faceModel = "Model Weight/opencv_face_detector_uint8.pb"
 
-ageProto = "modelNweight/age_deploy.prototxt"
-ageModel = "modelNweight/age_net.caffemodel"
+ageProto = "Model Weight/age_deploy.prototxt"
+ageModel = "Model Weight/age_net.caffemodel"
 
-genderProto = "modelNweight/gender_deploy.prototxt"
-genderModel = "modelNweight/gender_net.caffemodel"
+genderProto = "Model Weight/gender_deploy.prototxt"
+genderModel = "Model Weight/gender_net.caffemodel"
 
 MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
 ageList = ['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
